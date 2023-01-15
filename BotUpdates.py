1cbc4549ca32529c32a66b5e403103a5aa87748d
Lyohha <325alex.000@gnail.com>
2023-01-15 16:20:53 +0200

    Update 1.3.1

--- BotUpdates.py
+++ BotUpdates.py
@@ -6,8 +6,8 @@ from telegram.ext import Updater
 
 
 class BotUpdates:
-    version = '1.3.0'
-    description = 'Обновление до версии 1.3.0:\n- В списке аниме добавлена возможность просмотра списка всех серий аниме для скачивания. \n- В новостной ленте добавлено выбор качества для скачивания.'
+    version = '1.3.1'
+    description = 'Обновление до версии 1.3.1:\n- Добавлена пагинация в список серий для многосерийных аниме.'
 
     def __init__(self):
         updater = Updater(SecretInfo.TELEGRAM_HTTP_API_TOKEN, use_context=True)
